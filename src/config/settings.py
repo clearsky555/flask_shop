27f6b084dc5c0c4d7b71741ad815fe8d84863a48
clear <clarence.sky@yandex.ru>
2023-05-19 20:05:01 +0600

    add urls and new database settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'src.config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'flask_db',
+        'USER': 'flask_user',
+        'PASSWORD': 'allsafe1',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
